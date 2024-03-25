5e9ffc79b1b04bf3ced2b36c70b65ccfda83ebf4
root <2796070093@qq.com>
2024-03-25 17:35:01 +0800

    Initial commit

---
+++ src/main/java/com/glen/App.java
@@ -0,0 +1,13 @@
+package com.glen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
